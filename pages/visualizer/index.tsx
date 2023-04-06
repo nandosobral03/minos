863d2013c666bb180729374dc5ee31f740853170
Fernando Sobral <nandosobral03@gmail.com>
2023-04-05 21:50:22 -0300

    Small refactoring

--- pages/visualizer/index.tsx
+++ pages/visualizer/index.tsx
@@ -1,7 +1,7 @@
 import Options from "@/components/Options";
 import Layout from "@/components/Layout";
 import VisualizerComponent from "@/components/VisualizerContainer";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Algorithm, Steps } from "@/models/Algorithm.model";
 
 const Visualizer = () => {
@@ -13,69 +13,56 @@ const Visualizer = () => {
   const [steps, setSteps] = useState<Steps[]>([]);
   const [currentStep, _setCurrentStep] = useState(0);
   const [path, setPath] = useState<[number, number][]>([]);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      resize();
-    });
-    resize();
-  }, []);
-
-
-  const setCurrentStep = (step: number) => {
-    if(step == 0) _setCurrentStep(0);
-    if(step < 0 || step >= steps.length) return;
-    _setCurrentStep(step);
-  }
-
-  const resize = () => {
-    let realWidth = window.innerWidth - 250;
-    let realHeight = window.innerHeight * 0.95 - 48;
-    let cellSize = 25;
-
-    if (realWidth > 350) {
-      setWidth(Math.ceil(realWidth / cellSize));
-    }
-    if (realHeight > 200) {
-      setHeight(Math.ceil(realHeight / cellSize));
-    }
-  };
-
-  useEffect(() => {
-    resetMatrix();
-  }, [height, width]);
-
-  const resetMatrix = () => {
-    const newMatrix = Array.from({ length: height }, () =>
-      Array.from({ length: width }, () => 0)
-    );
-    newMatrix[0][0] = 3;
-    newMatrix[height - 1][width - 1] = 4;
-    setMatrix((prev) => newMatrix)
-    setSteps([]);
-  };
-
-  const clearVisited = () => {
-    const valuesToKeep = [3, 4, 5];
-    const newMatrix = matrix.map((row) => [...row.map((cell) => ( valuesToKeep.includes(cell) ? cell : 0))]);
-    setMatrix(newMatrix);
-  }
-
   const [matrix, setMatrix] = useState<number[][]>(
     Array.from({ length: height }, () => Array.from({ length: width }, () => 0))
   );
 
-  const changeMatrixValueForceSync = (x: number, y: number, value: number) => {
-    setMatrix((prev) => {
-      prev[x][y] = value;
-      return prev;
-    })
-  };
+  useEffect(() => {
+    const resize = () => {
+      const realWidth = window.innerWidth - 250;
+      const realHeight = window.innerHeight * 0.95 - 48;
+      const cellSize = 25;
+      console.log(realWidth, realHeight);
+      if (realWidth > 350) {
+        setWidth(Math.ceil(realWidth / cellSize));
+      }
+      if (realHeight > 200) {
+        setHeight(Math.ceil(realHeight / cellSize));
+      }
+    };
 
-  const changeMatrixValue = (x: number, y: number, value: number) => {
-    const newMatrix = matrix.map((row) => [...row]);
-    newMatrix[x][y] = value;
+    window.addEventListener("resize", resize);
+    resize();
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+
+  const resetMatrix = useCallback(() => {
+    const newMatrix = Array.from({ length: height }, () => Array.from({ length: width }, () => 0));
+    newMatrix[0][0] = 3;
+    newMatrix[height - 1][width - 1] = 4;
     setMatrix(newMatrix);
+    setSteps([]);
+  }, [height, width]);
+
+
+  useEffect(() => {
+    resetMatrix();
+  }, [resetMatrix])
+
+
+
+  const changeMatrixValue = (x: number, y: number, value: number, forceSync = false) => {
+    if (forceSync) {
+      setMatrix((prev) => {
+        prev[x][y] = value;
+        return prev;
+      });
+    } else {
+      const newMatrix = matrix.map((row) => [...row]);
+      newMatrix[x][y] = value;
+      setMatrix(newMatrix);
+    }
   };
 
   const runAlgorithm = () => {
@@ -98,39 +85,47 @@ const Visualizer = () => {
 
   useEffect(() => {
     setCurrentStep(steps.length == 0 ? 0 : steps.length - 1);
-  }, [steps])
+  }, [steps]);
+
+  
+  const setCurrentStep = (step: number) => {
+    if (step == 0) _setCurrentStep(0);
+    if (step < 0 || step >= steps.length) return;
+    _setCurrentStep(step);
+  };
 
 
   useEffect(() => {
+    const clearVisited = () => {
+      const valuesToKeep = [3, 4, 5];
+      const newMatrix = matrix.map((row) => row.map((cell) => (valuesToKeep.includes(cell) ? cell : 0)));
+      setMatrix(newMatrix);
+    };
+    
     const nextStep = steps[currentStep];
-    if(!nextStep) return;
+    if (!nextStep) return;
     clearVisited();
     let lastVisitedSteps = nextStep.visited;
     let current = nextStep.node;
     for (let elem of Array.from(lastVisitedSteps)) {
       const [x, y] = elem.split(",").map((e) => parseInt(e));
-      if(matrix[x][y] === 4 || matrix[x][y] === 3) continue;
-      changeMatrixValueForceSync(x, y, 1);
+      if (matrix[x][y] === 4 || matrix[x][y] === 3) continue;
+      changeMatrixValue(x, y, 1, true);
     }
 
     if (currentStep === steps.length - 1) {
       showFinalPath();
     } else {
       const [x, y] = current;
-      changeMatrixValueForceSync(x, y, 6);
+      changeMatrixValue(x, y, 6, true);
     }
   }, [currentStep]);
-
-
-
-
-
 
   const showFinalPath = () => {
     for (let elem of path) {
       const [x, y] = elem;
-      if(matrix[x][y] === 4 || matrix[x][y] === 3) continue;
-      changeMatrixValueForceSync(x, y, 2);
+      if (matrix[x][y] === 4 || matrix[x][y] === 3) continue;
+      changeMatrixValue(x, y, 2, true);
     }
   };
 
@@ -155,8 +150,8 @@ const Visualizer = () => {
           brush={brush}
           runAlgorithm={runAlgorithm}
           reset={() => resetMatrix()}
-          nextStep = {() => setCurrentStep(currentStep + 1)}
-          previousStep = {() => setCurrentStep(currentStep - 1)}
+          nextStep={() => setCurrentStep(currentStep + 1)}
+          previousStep={() => setCurrentStep(currentStep - 1)}
         />
         <VisualizerComponent
           currentMatrix={matrix}
